c344be9c13212665eb7fc1ac77dcee1c770210c2
joseph_ctu <sephxd1234@gmail.com>
2017-11-04 16:15:19 +0100

    feat: categories reducer
    Returns the categories received from an async response, as a piece of
    state

---
+++ frontend/src/reducers/categories.js
@@ -0,0 +1,10 @@
+import * as types from "../constants";
+
+export default function categories(state = [], action) {
+  switch (action.type) {
+    case types.LOAD_CATEGORIES:
+      return action.payload;
+    default:
+      return state;
+  }
+}
