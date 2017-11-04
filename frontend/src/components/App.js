1340eed688148765cd8ecd11fe082e6ae8ca6148
joseph_ctu <sephxd1234@gmail.com>
2017-11-04 16:14:29 +0100

    feat: connected App.js to redux

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,14 +1,33 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getAllCategories } from "../actions";
+import "./App.css";
 
 class App extends Component {
+  componentDidMount() {
+    this.props.getAllCategories();
+  }
   render() {
+    const { categories } = this.props;
     return (
       <div className="App">
-       Let's start
+        Categories:{" "}
+        {categories &&
+          categories.map((cat, i) => <span key={i}> {cat.name} </span>)}
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    categories: state.categories,
+    counter: state.counter
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return { getAllCategories: () => dispatch(getAllCategories) };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
