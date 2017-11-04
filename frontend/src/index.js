a7bcd01f811213b52a712eccdb863c77d7122791
joseph_ctu <sephxd1234@gmail.com>
2017-11-04 16:11:55 +0100

    Feat: Implemented redux store, provider and routes
    A redux store is created. The React-redux provider wraps the routes.
    
    Numerous imports are added: reducer, reducer, routes, providers,
    middleware, bootstrap, etc.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore, compose } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import rootReducer from "./reducers";
+import routes from "./routes";
+import registerServiceWorker from "./registerServiceWorker";
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+  : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk, logger)
+);
+
+const store = createStore(rootReducer, enhancer);
+
+ReactDOM.render(
+  <Provider store={store}>{routes}</Provider>,
+  document.getElementById("root")
+);
+
 registerServiceWorker();
