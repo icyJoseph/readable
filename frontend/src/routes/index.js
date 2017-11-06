a4eabb0dc706fb23a3e3ed2a73e50cefefc3876b
joseph_ctu <sephxd1234@gmail.com>
2017-11-06 08:54:26 +0100

    feat: Added Header and wrapped routes for main view and category viewer
    The header component is now present in all views. Routes are also wrapped,
    because BrowserRouter can only have one child.
    
    Route with params is included for category viewers.

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,10 +1,17 @@
 import React from "react";
-import { BrowserRouter, Route} from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import App from "../components/App";
+import Category from "../components/Category";
+import Header from "../components/Header";
+
 
 const routes = (
   <BrowserRouter>
+    <div>
+      <Header />
       <Route exact path="/" component={App} />
+      <Route path="/:cat" component={Category} />
+    </div>
   </BrowserRouter>
 );
 
