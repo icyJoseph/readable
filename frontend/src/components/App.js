317f3ef64557011db264071745ebfd0d4b0f77bb
joseph_ctu <sephxd1234@gmail.com>
2017-11-09 10:44:16 +0100

    feat: Added Category children to render all posts

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
+import Category from "./Category";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Main View
+        <div>
+          <Category />
+        </div>
       </div>
     );
   }
