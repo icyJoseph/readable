0d14ffac76ece558a02ec8f70e2188c77a6f4992
joseph_ctu <sephxd1234@gmail.com>
2017-11-09 10:54:47 +0100

    feat: Category component added as a container for posts

--- frontend/src/components/Category.js
+++ frontend/src/components/Category.js
@@ -1,10 +1,39 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getPosts } from "../actions";
+import { capitalizer } from "../utils/helpers";
+
+import PostsList from "./PostsList";
 
 class Category extends Component {
+  componentDidMount() {
+    const { match } = this.props;
+    this.props.getPosts(match ? match.params.cat : "");
+  }
+
   render() {
-    const { match: { params: {cat} } } = this.props;
-    return <div>Category Viewer: You are viewing {cat}</div>;
+    const { match, posts } = this.props;
+    const cat = match ? match.params.cat : "";
+    return (
+      <div className="App">
+        Category Viewer: You are viewing {cat ? capitalizer(cat) : "Main view"}{" "}
+        which has {posts.length} posts
+        <PostsList posts={posts} />
+      </div>
+    );
   }
 }
 
-export default Category;
+function mapStateToProps(state) {
+  return {
+    posts: state.posts
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    getPosts: cat => dispatch(getPosts(cat))
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Category);
