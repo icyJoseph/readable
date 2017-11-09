98d3f1817b811f08f9e5d4c557be709525047957
joseph_ctu <sephxd1234@gmail.com>
2017-11-09 11:00:13 +0100

    chore: added action types and special character helpers

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,2 +1,11 @@
 export const GET_CATEGORIES = "get_all_categories";
 export const LOAD_CATEGORIES = "load_all_categories";
+
+export const GET_POSTS = "get_posts";
+export const LOAD_POSTS = "load_posts";
+
+export const GET_COMMENTS = "get_comments";
+export const LOAD_COMMENTS = "load_comments";
+
+export const UP_VOTE = "↑";
+export const DOWN_VOTE = "↓";
