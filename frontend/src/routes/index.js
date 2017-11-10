15779200f1d5bf929601780e80eccf4e89131509
joseph_ctu <sephxd1234@gmail.com>
2017-11-10 10:16:25 +0100

    fix: Added exact route to category viewer

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -3,14 +3,15 @@ import { BrowserRouter, Route } from "react-router-dom";
 import App from "../components/App";
 import Category from "../components/Category";
 import Header from "../components/Header";
-
+import Post from "../components/Post";
 
 const routes = (
   <BrowserRouter>
     <div>
       <Header />
       <Route exact path="/" component={App} />
-      <Route path="/:cat" component={Category} />
+      <Route exact path="/:cat" component={Category} />
+      <Route path="/:cat/:id" component={Post} />
     </div>
   </BrowserRouter>
 );
