14919f93ef6a18394ca11aa2ca7992769fc5aa3d
joseph_ctu <sephxd1234@gmail.com>
2017-11-10 10:18:53 +0100

    chore: added constants related to fetching posts

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -7,5 +7,8 @@ export const LOAD_POSTS = "load_posts";
 export const GET_COMMENTS = "get_comments";
 export const LOAD_COMMENTS = "load_comments";
 
+export const GET_POST_BY_ID = "get_post_by_id";
+export const LOAD_POST_BY_ID = "load_post_by_id";
+
 export const UP_VOTE = "↑";
 export const DOWN_VOTE = "↓";
