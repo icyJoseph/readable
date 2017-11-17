ca1c35340490f19303fb447fc87c53a8860d85a8
joseph_ctu <sephxd1234@gmail.com>
2017-11-17 15:06:11 +0100

    feat: load comment by id reducer

--- frontend/src/reducers/comments.js
+++ frontend/src/reducers/comments.js
@@ -4,6 +4,13 @@ export default function comments(state = [], action) {
   switch (action.type) {
     case types.LOAD_COMMENTS:
       return action.payload;
+    case types.LOAD_COMMENT_BY_ID:
+      //filter state to remove the comment in case it already exists
+      const filteredState = state.filter(
+        comment => comment.id !== action.payload.id
+      );
+      // Return an array which adds the comment to the filtered state
+      return filteredState.concat(action.payload);
     default:
       return state;
   }
