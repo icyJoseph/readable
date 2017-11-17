a5afe2eb17616c47f5f51b482bb7869c66fffca5
joseph_ctu <sephxd1234@gmail.com>
2017-11-17 15:06:25 +0100

    feat: added sort reducer to combineReducers

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import categories from "./categories";
 import comments from "./comments";
 import posts from "./posts";
+import sort from "./sort";
 
 export default combineReducers({
   categories,
   comments,
-  posts
+  posts,
+  sort
 });
