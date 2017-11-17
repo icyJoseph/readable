dd1953ed411b3fe417c1024072f095efb1c5ea63
joseph_ctu <sephxd1234@gmail.com>
2017-11-17 15:05:26 +0100

    feat: remove post from state if already there and concat the payload

--- frontend/src/reducers/posts.js
+++ frontend/src/reducers/posts.js
@@ -5,7 +5,10 @@ export default function posts(state = [], action) {
     case types.LOAD_POSTS:
       return action.payload;
     case types.LOAD_POST_BY_ID:
-      return Object.assign([], [action.payload], state);
+      //filter state to remove the post in case it already exists
+      const filteredState = state.filter(post => post.id !== action.payload.id);
+      // Return an array which adds the post to the filtered state
+      return filteredState.concat(action.payload);
     default:
       return state;
   }
