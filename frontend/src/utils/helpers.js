760fd65e4c5ae78ba445d574f8eb28bb90f769c2
joseph_ctu <sephxd1234@gmail.com>
2017-11-17 09:24:26 +0100

    feat: Function to sort arrays by a given rule. Metadata generator.

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -1,4 +1,35 @@
+import uuid from "uuid/v1";
+import * as types from "../constants";
+
 // Changes first letter of a word from lower case to upper case. Returns a new word.
 export const capitalizer = word => {
-    return word.charAt(0).toUpperCase() + word.slice(1);
-}
+  return word.charAt(0).toUpperCase() + word.slice(1);
+};
+
+// Sorts an array according to a given rule
+export const sortArr = (arr, rule) => {
+  let property = "voteScore";
+  switch (rule) {
+    case types.MOST_POPULAR:
+      property = "voteScore";
+      return arr.slice().sort((a, b) => b[property] - a[property]);
+    case types.MOST_HATED:
+      property = "voteScore";
+      return arr.slice().sort((a, b) => a[property] - b[property]);
+    case types.RECENT:
+      property = "timestamp";
+      return arr.slice().sort((a, b) => b[property] - a[property]);
+    case types.OLDEST:
+      property = "timestamp";
+      return arr.slice().sort((a, b) => a[property] - b[property]);
+    default:
+      return arr.slice().sort((a, b) => b[property] - a[property]);
+  }
+};
+
+export const generateMeta = () => {
+  return {
+    id: uuid(),
+    timestamp: Date.now()
+  };
+};
