818a11c068bf1b2d88c3d0d485b0f2e659ffd550
joseph_ctu <sephxd1234@gmail.com>
2017-11-17 09:26:55 +0100

    feat: PostCreator, Comment Fetch, Vote action.
    Also refactored some of the actions to be able to reuse them for both
    posts and comments

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -25,8 +25,9 @@ export const getAllCategories = dispatch => {
 export const getPosts = cat => {
   return function(dispatch) {
     dispatch({ type: types.GET_POSTS });
-    const query = cat === "" ? "posts" : `${cat}/posts`;
-    axios.get(`${apiEndPoint}/${query}`, config).then(res => {
+    const query =
+      cat === "" ? `${apiEndPoint}/posts` : `${apiEndPoint}/${cat}/posts`;
+    axios.get(query, config).then(res => {
       dispatch({
         type: types.LOAD_POSTS,
         payload: res.data
@@ -39,10 +40,24 @@ export const getPosts = cat => {
 export const getPostByID = id => {
   return function(dispatch) {
     dispatch({ type: types.GET_POST_BY_ID });
-    const query = `posts/${id}`;
-    axios.get(`${apiEndPoint}/${query}`, config).then(res => {
+    const query = `${apiEndPoint}/posts/${id}`;
+    axios.get(query, config).then(res => {
       dispatch({
         type: types.LOAD_POST_BY_ID,
+        payload: res.data
+      });
+    });
+  };
+};
+
+//Action creator to get data from a single comment by its id
+export const getCommentByID = id => {
+  return function(dispatch) {
+    dispatch({ type: types.GET_COMMENT_BY_ID });
+    const query = `${apiEndPoint}/comment/${id}`;
+    axios.get(query, config).then(res => {
+      dispatch({
+        type: types.LOAD_COMMENT_BY_ID,
         payload: res.data
       });
     });
@@ -53,12 +68,74 @@ export const getPostByID = id => {
 export const getComments = id => {
   return function(dispatch) {
     dispatch({ type: types.GET_COMMENTS });
-    const query = `posts/${id}/comments`;
-    axios.get(`${apiEndPoint}/${query}`, config).then(res => {
+    const query = `${apiEndPoint}/posts/${id}/comments`;
+    axios.get(query, config).then(res => {
       dispatch({
         type: types.LOAD_COMMENTS,
         payload: res.data
       });
     });
+  };
+};
+
+// Action creator to upVote or downVote an original post or a comment
+export const vote = (id, option, type) => {
+  const data = { option: option };
+  switch (type) {
+    case types.POST:
+      return function(dispatch) {
+        dispatch({ type: types.VOTE_POST });
+        const query = `${apiEndPoint}/posts/${id}`;
+        axios.post(query, data, config).then(res => {
+          dispatch({
+            type: types.LOAD_POST_BY_ID,
+            payload: res.data
+          });
+        });
+      };
+    case types.COMMENT:
+      return function(dispatch) {
+        dispatch({ type: types.VOTE_COMMENT });
+        const query = `${apiEndPoint}/comments/${id}`;
+        axios.post(query, data, config).then(res => {
+          dispatch({
+            type: types.LOAD_COMMENT_BY_ID,
+            payload: res.data
+          });
+        });
+      };
+    default:
+      return null;
+  }
+};
+
+//Action creator to create  post in the server
+export const createPost = post => {
+  return function(dispatch) {
+    dispatch({ type: types.CREATE_POST });
+    if (post.parentId) {
+      const query = `${apiEndPoint}/comments`;
+      axios.post(query, post, config).then(res => {
+        dispatch({
+          type: types.LOAD_COMMENT_BY_ID,
+          payload: res.data
+        });
+      });
+    } else {
+      const query = `${apiEndPoint}/posts`;
+      axios.post(query, post, config).then(res => {
+        dispatch({
+          type: types.LOAD_POST_BY_ID,
+          payload: res.data
+        });
+      });
+    }
+  };
+};
+
+//Action to change the sorting mechanism
+export const changeSort = mechanism => {
+  return {
+    type: mechanism
   };
 };
