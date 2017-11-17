609b56050490fc1973e033adac20bb4f3c6cd8d2
joseph_ctu <sephxd1234@gmail.com>
2017-11-17 15:03:36 +0100

    feat: connected to vote component and link to edit/delete

--- frontend/src/components/GenericDisplay.js
+++ frontend/src/components/GenericDisplay.js
@@ -5,70 +5,121 @@ import {
   ListGroupItemHeading,
   ListGroupItemText
 } from "reactstrap";
-import { UP_VOTE, DOWN_VOTE } from "../constants";
+
+import Vote from "./Vote";
+
+import { UP_VOTE, DOWN_VOTE, POST, COMMENT } from "../constants";
 import { capitalizer } from "../utils/helpers";
 
 class GenericDisplay extends Component {
   // TODO: Use timeStamp
-  displayOP(id, title, body, author, category, voteScore, commentCount) {
+  displayOP({
+    id,
+    timestamp,
+    title,
+    body,
+    author,
+    category,
+    voteScore,
+    commentCount
+  }) {
+    const date = new Date(timestamp);
+    const formatedDate = date.format("dd.mm.yyyy");
+    console.log(formatedDate);
     return (
-      <ListGroupItem>
+      <ListGroupItem style={margins}>
         <ListGroupItemHeading>
           <NavLink to={`/${category}/${id}`}>{title}</NavLink>{" "}
         </ListGroupItemHeading>
         <ListGroupItemText>{body}</ListGroupItemText>
         <ListGroupItemText>by: {author}</ListGroupItemText>
+        <ListGroupItemText>Category: {capitalizer(category)}</ListGroupItemText>
+        <ListGroupItemText>Comments: {commentCount}</ListGroupItemText>
         <ListGroupItemText>
-          <span>Category: {capitalizer(category)}</span> {" "}
-          <span>Comments: {commentCount}</span> {" "}
-          <span>
-            Votes:{" "}
-            {voteScore > 0
-              ? `${voteScore} ${UP_VOTE}`
-              : voteScore === 0 ? "" : `${voteScore} ${DOWN_VOTE}`}
-          </span>
+          Votes:{" "}
+          {voteScore > 0 ? (
+            <span style={upVote}>
+              {voteScore} {UP_VOTE}
+            </span>
+          ) : voteScore === 0 ? (
+            "0"
+          ) : (
+            <span style={downVote}>
+              {voteScore} {DOWN_VOTE}
+            </span>
+          )}
         </ListGroupItemText>
+        <ListGroupItemText>
+          <NavLink to={`/editpost/${id}`} style={tooling}>
+            Edit
+          </NavLink>
+          <span style={tooling}>Delete</span>
+        </ListGroupItemText>
+        <Vote id={id} type={POST} />
       </ListGroupItem>
     );
   }
 
   // TODO: Use timeStamp
-  displayComment(author, body, voteScore) {
+  displayComment({ id, author, body, voteScore }) {
     return (
-      <ListGroupItem>
+      <ListGroupItem style={margins}>
         <ListGroupItemText>{author} says:</ListGroupItemText>
         <ListGroupItemText>{body}</ListGroupItemText>
         <ListGroupItemText>
-          <span>
-            Votes:{" "}
-            {voteScore > 0
-              ? `${voteScore} ${UP_VOTE}`
-              : voteScore === 0 ? "" : `${voteScore} ${DOWN_VOTE}`}
-          </span>
+          Votes:{" "}
+          {voteScore > 0 ? (
+            <span style={upVote}>
+              {voteScore} {UP_VOTE}
+            </span>
+          ) : voteScore === 0 ? (
+            "0"
+          ) : (
+            <span style={downVote}>
+              {voteScore} {DOWN_VOTE}
+            </span>
+          )}
         </ListGroupItemText>
+        <ListGroupItemText>
+          <NavLink to={`/editcomment/${id}`} style={tooling}>
+            Edit
+          </NavLink>
+          <span style={tooling}>Delete</span>
+        </ListGroupItemText>
+        <Vote id={id} type={COMMENT} />
       </ListGroupItem>
     );
   }
 
   render() {
-    const {
-      post: { id, title, body, author, category, voteScore, commentCount }
-    } = this.props;
+    const { post } = this.props;
     const postIsAComment = this.props.post.parentId;
     if (postIsAComment) {
-      return this.displayComment(author, body, voteScore);
+      return this.displayComment(post);
     } else {
-      return this.displayOP(
-        id,
-        title,
-        body,
-        author,
-        category,
-        voteScore,
-        commentCount
-      );
+      return this.displayOP(post);
     }
   }
 }
+
+const upVote = {
+  color: "green"
+};
+
+const downVote = {
+  color: "red"
+};
+
+const margins = {
+  marginBottom: "7px",
+  marginTop: "7px"
+};
+
+const tooling = {
+  cursor: "pointer",
+  fontSize: "9pt",
+  margin: "10px",
+  textDecoration: "underline"
+};
 
 export default GenericDisplay;
