5b2c38404daeb91916806c0b26505b8c114503cb
joseph_ctu <sephxd1234@gmail.com>
2017-11-17 14:56:28 +0100

    chore:Added constants for actions and displayed text

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -7,8 +7,39 @@ export const LOAD_POSTS = "load_posts";
 export const GET_COMMENTS = "get_comments";
 export const LOAD_COMMENTS = "load_comments";
 
+export const GET_COMMENT_BY_ID = "get_comment_by_id";
+export const LOAD_COMMENT_BY_ID = "get_comment_by_id";
+
 export const GET_POST_BY_ID = "get_post_by_id";
 export const LOAD_POST_BY_ID = "load_post_by_id";
 
+export const VOTE_POST = "vote_post";
+export const VOTE_COMMENT = "vote_comment";
+
+export const OPTION_UPVOTE = "upVote";
+export const OPTION_DOWNVOTE = "downVote";
+
+export const CREATE_POST = "create_post";
+
+// Constants Strings in the app
+
 export const UP_VOTE = "↑";
 export const DOWN_VOTE = "↓";
+
+export const POST = "Post";
+export const COMMENT = "Comment";
+
+export const MOST_POPULAR = "Most Popular";
+export const MOST_HATED = "Most Hated";
+export const RECENT = "Recent";
+export const OLDEST = "Oldest";
+
+export const TITLE = "Title";
+export const BODY = "Body";
+export const CATEGORY = "Category";
+export const SUBMIT = "Submit";
+export const AUTHOR = "Author";
+export const YOUR_COMMENT = "Your Comment";
+export const SELECT_HOLDER = "Select a category...";
+
+export const BRAND_TITLE = "Readable Home";
