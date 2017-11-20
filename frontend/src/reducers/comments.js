24bbeb8d2e1852f5ee8de89846a5d26387d1a63b
joseph_ctu <sephxd1234@gmail.com>
2017-11-20 19:49:45 +0100

    feat: Reducer now filters out elements marked as deleted

--- frontend/src/reducers/comments.js
+++ frontend/src/reducers/comments.js
@@ -3,14 +3,20 @@ import * as types from "../constants";
 export default function comments(state = [], action) {
   switch (action.type) {
     case types.LOAD_COMMENTS:
-      return action.payload;
+      return action.payload.filter(
+        comment => !comment.deleted && !comment.parentDeleted
+      );
     case types.LOAD_COMMENT_BY_ID:
       //filter state to remove the comment in case it already exists
       const filteredState = state.filter(
         comment => comment.id !== action.payload.id
       );
       // Return an array which adds the comment to the filtered state
-      return filteredState.concat(action.payload);
+      return action.payload.deleted
+        ? filteredState
+        : filteredState.concat(action.payload);
+    case types.NO_COMMENT:
+      return state.concat(action.payload);
     default:
       return state;
   }
