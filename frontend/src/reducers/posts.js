0e878b4a71a59a5d9811f18e047f111e89eaa641
joseph_ctu <sephxd1234@gmail.com>
2017-11-20 19:50:23 +0100

    feat: reducer now filters posts marked as deleted

--- frontend/src/reducers/posts.js
+++ frontend/src/reducers/posts.js
@@ -3,12 +3,16 @@ import * as types from "../constants";
 export default function posts(state = [], action) {
   switch (action.type) {
     case types.LOAD_POSTS:
-      return action.payload;
+      return action.payload.filter(post => !post.deleted);
     case types.LOAD_POST_BY_ID:
       //filter state to remove the post in case it already exists
       const filteredState = state.filter(post => post.id !== action.payload.id);
       // Return an array which adds the post to the filtered state
-      return filteredState.concat(action.payload);
+      return action.payload.deleted
+        ? filteredState
+        : filteredState.concat(action.payload);
+    case types.NO_POST:
+      return state.concat(action.payload);
     default:
       return state;
   }
